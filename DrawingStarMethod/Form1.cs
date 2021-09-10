8fdcaec2f255030176b943dd6ea0620d263c5ab0
P1 <P1@TK_018>
2021-09-10 10:54:29 -0400

    Made Draw and Fill buttons work.

--- DrawingStarMethod/Form1.cs
+++ DrawingStarMethod/Form1.cs
@@ -30,14 +30,41 @@ namespace DrawingStarMethod
 
             Graphics g = this.CreateGraphics();
             Pen blackPen = new Pen(Color.Black);
-            
 
+            PointF[] star = new PointF[10];
+            star[0] = new PointF(57, 3);
+            star[1] = new PointF(70, 43);
+            star[2] = new PointF(106, 43);
+            star[3] = new PointF(77, 69);
+            star[4] = new PointF(90, 109);
+            star[5] = new PointF(57, 84);
+            star[6] = new PointF(24, 109);
+            star[7] = new PointF(36, 69);
+            star[8] = new PointF(1, 43);
+            star[9] = new PointF(44, 43);
+
+            g.DrawPolygon(blackPen, star);
         }
 
 
         private void fillButton_Click(object sender, EventArgs e)
         {
+            Graphics g = this.CreateGraphics();
+            SolidBrush blueBrush = new SolidBrush(Color.AliceBlue);
 
+            PointF[] star = new PointF[10];
+            star[0] = new PointF(57, 3);
+            star[1] = new PointF(70, 43);
+            star[2] = new PointF(106, 43);
+            star[3] = new PointF(77, 69);
+            star[4] = new PointF(90, 109);
+            star[5] = new PointF(57, 84);
+            star[6] = new PointF(24, 109);
+            star[7] = new PointF(36, 69);
+            star[8] = new PointF(1, 43);
+            star[9] = new PointF(44, 43);
+
+            g.FillPolygon(blueBrush, star);
         }
 
         public void DrawStar(Pen starPen, float x, float y, float pixels)
